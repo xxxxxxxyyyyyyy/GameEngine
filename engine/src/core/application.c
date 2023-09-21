4794aa23235bb7f3e51695fa0fb55cafc4c86724
dreamersofdreams <7959086+dreamersofdreams@user.noreply.gitee.com>
2023-09-21 18:17:57 +0800

    basic input system
    basic input system without os binding.

--- engine/src/core/application.c
+++ engine/src/core/application.c
@@ -6,6 +6,7 @@
 #include "platform/platform.h"
 #include "core/kmemory.h"
 #include "core/event.h"
+#include "core/input.h"
 
 typedef struct application_state {
 	game* game_inst;
@@ -32,6 +33,7 @@ b8 application_create(game* game_inst) {
 
 	// Initialize Subsystems.
 	initialize_logging();
+	input_initialize();
 
 	// TODO: Remove this.
 	KFATAL("a test message: %f", 3.14f);
@@ -90,12 +92,19 @@ b8 application_run() {
 				app_state.is_running = FALSE;
 				break;
 			}
+
+			// NOTE: input update/state copying should always be handled 
+			// after any input should be recorded; 
+			// as a safety, input is the last thing to be updated before 
+			// this frame ends. update input used to next frame
+			input_update(0);
 		}
 	}
 
 	app_state.is_running = FALSE;
 
 	event_shutdown();
+	input_shutdown();
 
 	platform_shutdown(&app_state.platform);
 
